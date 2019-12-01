847e6f68375730bf70bf3687191096074388e3a3
gregwhitaker <gwhitake@gmail.com>
2019-12-01 09:34:40 -0800

    Doing work

--- src/main/java/example/employee/EmployeeApplication.java
+++ src/main/java/example/employee/EmployeeApplication.java
@@ -1,8 +1,12 @@
 package example.employee;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class EmployeeApplication {
 
     public static void main(String... args) {
-
+        SpringApplication.run(EmployeeApplication.class, args);
     }
 }
