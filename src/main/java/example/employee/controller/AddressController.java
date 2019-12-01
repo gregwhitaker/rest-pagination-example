74bd414c606819d3c780283b4dc73d387bd4ff25
gregwhitaker <gwhitake@gmail.com>
2019-12-01 13:48:34 -0800

    Doing work

--- src/main/java/example/employee/controller/AddressController.java
+++ src/main/java/example/employee/controller/AddressController.java
@@ -19,6 +19,12 @@ public class AddressController {
     @Autowired
     private EmployeeService employeeService;
 
+    /**
+     * Gets the employee's home address.
+     *
+     * @param employeeId employee id
+     * @return employee's home address if available
+     */
     @GetMapping(value = "/employees/{id}/addresses/home",
                 produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getHomeAddressForEmployee(@PathVariable("id") long employeeId) {
@@ -31,6 +37,12 @@ public class AddressController {
         }
     }
 
+    /**
+     * Gets the employee's work address.
+     *
+     * @param employeeId employee id
+     * @return employee's work address if available
+     */
     @GetMapping(value = "/employees/{id}/addresses/work",
                 produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getWorkAddressForEmployee(@PathVariable("id") long employeeId) {
