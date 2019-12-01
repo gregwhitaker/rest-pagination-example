88f644b7996b5d56b023569f934ddc3dd029b23b
gregwhitaker <gwhitake@gmail.com>
2019-12-01 09:35:40 -0800

    Doing work

---
+++ src/main/java/example/employee/controller/EmployeeController.java
@@ -0,0 +1,10 @@
+package example.employee.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
+}
